626956b5cbde92a084d2a068a2f14aa15f3a16ee
Luigi Quattrociocchi <lquatt2@gmail.com>
2020-09-06 00:20:25 -0400

    re-write

--- js/dark.js
+++ js/dark.js
@@ -1,33 +1,33 @@
 const darkSwitch = document.getElementById('darkSwitch');
+let mainNav = document.getElementById("main-nav");
+
 window.addEventListener('load', () => {
 	if (darkSwitch) {
 		initTheme();
-		darkSwitch.addEventListener('change', () => { resetTheme(); });
+		darkSwitch.addEventListener('change', setTheme);
 	}
 });
 
-function initTheme() {
-	const darkThemeSelected = localStorage.getItem('darkSwitch') !== null && localStorage.getItem('darkSwitch') === 'dark';
-	darkSwitch.checked = darkThemeSelected;
-	if (darkThemeSelected) {
+const initTheme = () => {
+	if (darkSwitch.checked = localStorage.getItem('darkSwitch') === 'dark') {
 		document.body.setAttribute('data-theme', 'dark');
-		document.getElementById("main-nav").className = document.getElementById("main-nav").className.replace(/\blight\b/g, "dark");
+		mainNav.className = mainNav.className.replace(/\blight\b/g, "dark");
 	}
 	else {
 		document.body.removeAttribute('data-theme');
-		document.getElementById("main-nav").className = document.getElementById("main-nav").className.replace(/\bdark\b/g, "light");
+		mainNav.className = mainNav.className.replace(/\bdark\b/g, "light");
 	}
 }
 
-function resetTheme() {
+const setTheme = () => {
 	if (darkSwitch.checked) {
 		document.body.setAttribute('data-theme', 'dark');
-		document.getElementById("main-nav").className = document.getElementById("main-nav").className.replace(/\blight\b/g, "dark");
+		mainNav.className = mainNav.className.replace(/\blight\b/g, "dark");
 		localStorage.setItem('darkSwitch', 'dark');
 	}
 	else {
 		document.body.removeAttribute('data-theme');
-		document.getElementById("main-nav").className = document.getElementById("main-nav").className.replace(/\bdark\b/g, "light");
+		mainNav.className = mainNav.className.replace(/\bdark\b/g, "light");
 		localStorage.removeItem('darkSwitch');
 	}
 }
