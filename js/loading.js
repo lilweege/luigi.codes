efa6c856c0f68ec65427a8157dbe280bf71a59b3
Luigi Quattrociocchi <lquatt2@gmail.com>
2020-09-07 01:54:21 -0400

    image loading optimization

--- js/loading.js
+++ js/loading.js
@@ -1,3 +1,10 @@
 window.addEventListener('load', () => {
 	document.getElementById('loading-screen').className = "hidden";
+	for (let low of document.images) {
+		let full = document.createElement('img');
+		full.src = low.src.replace(/\blow\b/g, "full");
+		full.onload = function() {
+			low.src = this.src;
+		}
+	}
 });
