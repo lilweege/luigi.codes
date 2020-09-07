c79211a17f11854fbcfd4a8af949d9692d9fec6c
Luigi Quattrociocchi <lquatt2@gmail.com>
2020-09-07 14:58:31 -0400

    concise logic

--- js/dark.js
+++ js/dark.js
@@ -3,31 +3,23 @@ const mainNav = document.getElementById("main-nav");
 
 window.addEventListener('load', () => {
 	if (darkSwitch) {
-		initTheme();
+		darkSwitch.checked = localStorage.getItem('darkSwitch') === 'dark';
+		setTheme();
 		darkSwitch.addEventListener('change', setTheme);
 	}
 });
 
-const initTheme = () => {
-	if (darkSwitch.checked = localStorage.getItem('darkSwitch') === 'dark') {
-		document.body.setAttribute('data-theme', 'dark');
-		mainNav.className = mainNav.className.replace(/\blight\b/g, "dark");
-	}
-	else {
-		document.body.removeAttribute('data-theme');
-		mainNav.className = mainNav.className.replace(/\bdark\b/g, "light");
-	}
-}
-
 const setTheme = () => {
 	if (darkSwitch.checked) {
-		document.body.setAttribute('data-theme', 'dark');
-		mainNav.className = mainNav.className.replace(/\blight\b/g, "dark");
 		localStorage.setItem('darkSwitch', 'dark');
+		document.body.setAttribute('data-theme', 'dark');
+		mainNav.classList.remove("navbar-light");
+		mainNav.classList.add("navbar-dark");
 	}
 	else {
-		document.body.removeAttribute('data-theme');
-		mainNav.className = mainNav.className.replace(/\bdark\b/g, "light");
 		localStorage.removeItem('darkSwitch');
+		document.body.removeAttribute('data-theme');
+		mainNav.classList.remove("navbar-dark");
+		mainNav.classList.add("navbar-light");
 	}
 }
