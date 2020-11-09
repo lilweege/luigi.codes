0cde7101ca2c85c8f030e4f6e853ee59f67ba822
Luigi Quattrociocchi <lquatt2@gmail.com>
2020-11-09 03:18:46 -0500

    portfolio: pdf viewer iframe

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,22 +1,20 @@
 window.addEventListener('load', () => {
-	const subpageSelectors = document.getElementsByClassName("list-group")
 	const dropdownSelectors = document.getElementsByClassName("nav-dropdown")
-	for (const selector of subpageSelectors) {
-		for (const link of selector.children) {
-			link.addEventListener("click", e => {
-				e.preventDefault()
-				for (const link of selector.children)
-					link.classList.remove("active")
-			})
-		}
-	}
 	for (const selector of dropdownSelectors) {
 		for (const link of selector.children) {
 			link.addEventListener("click", e => {
-				let a = document.getElementById(link.dataset.invocable);
-				if (a)
-					a.click()
+				let a = document.getElementById(link.dataset.invocable)
+				if (a) a.click()
 			})
 		}
+	}
+	const pdfSelectors1 = document.getElementsByClassName("DP-1-pdf-selector")
+	const pdfFrame1 = document.getElementById("DP-1-frame")
+	const pdfLink1 = document.getElementById("DP-1-link")
+	for (const selector of pdfSelectors1) {
+		selector.addEventListener("click", e => {
+			pdfFrame1.src = selector.dataset.src
+			pdfLink1.href = selector.dataset.src
+		})
 	}
 })
