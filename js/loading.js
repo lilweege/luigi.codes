110ca819b8cb883df5be4bdcfa2bfc923b07f315
Luigi Quattrociocchi <lquatt2@gmail.com>
2020-11-14 01:25:13 -0500

    added another new section

--- js/loading.js
+++ js/loading.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
 	document.getElementById('loading-screen').className = "hidden";
 	for (let low of document.images) {
 		let full = document.createElement('img');
-		full.src = low.src.replace(/\blow\b/g, "full");
+		full.src = low.src.replace(/\b\/low\/\b/g, "/full/");
 		full.onload = function() {
 			low.src = this.src;
 		}
