bd8e0a8137f8aab0e5411065f75710a708701a1c
Luigi Quattrociocchi <lquatt2@gmail.com>
2021-01-14 23:06:17 -0500

    fix links, add new project

--- js/portfolio.js
+++ js/portfolio.js
@@ -8,6 +8,8 @@ window.addEventListener('load', () => {
 			})
 		}
 	}
+
+	// TODO: this system should be improved
 	const pdfSelectors1 = document.getElementsByClassName("DP-1-pdf-selector")
 	const pdfFrame1 = document.getElementById("DP-1-frame")
 	const pdfLink1 = document.getElementById("DP-1-link")
@@ -15,6 +17,15 @@ window.addEventListener('load', () => {
 		selector.addEventListener("click", e => {
 			pdfFrame1.src = selector.dataset.src
 			pdfLink1.href = selector.dataset.src
+		})
+	}
+	const pdfSelectors2 = document.getElementsByClassName("DP-2-pdf-selector")
+	const pdfFrame2 = document.getElementById("DP-2-frame")
+	const pdfLink2 = document.getElementById("DP-2-link")
+	for (const selector of pdfSelectors2) {
+		selector.addEventListener("click", e => {
+			pdfFrame2.src = selector.dataset.src
+			pdfLink2.href = selector.dataset.src
 		})
 	}
 })
