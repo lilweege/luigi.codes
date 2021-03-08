44fc8b4d72676d9e771384679130b8f3710058ca
Luigi Quattrociocchi <lquatt2@gmail.com>
2021-03-07 19:40:07 -0500

    add project 3
    I really should improve the way I am handling the viewing and selection of these documents

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,4 @@
+// this is still very bad
 window.addEventListener('load', () => {
 	const dropdownSelectors = document.getElementsByClassName("nav-dropdown")
 	for (const selector of dropdownSelectors) {
@@ -9,23 +10,47 @@ window.addEventListener('load', () => {
 		}
 	}
 
-	// TODO: this system should be improved
 	const pdfSelectors1 = document.getElementsByClassName("DP-1-pdf-selector")
 	const pdfFrame1 = document.getElementById("DP-1-frame")
 	const pdfLink1 = document.getElementById("DP-1-link")
 	for (const selector of pdfSelectors1) {
-		selector.addEventListener("click", e => {
-			pdfFrame1.src = selector.dataset.src
-			pdfLink1.href = selector.dataset.src
-		})
+		if (selector.dataset.src) {
+			selector.addEventListener("click", e => {
+				pdfFrame1.src = selector.dataset.src
+				pdfLink1.href = selector.dataset.src
+			})
+		}
 	}
 	const pdfSelectors2 = document.getElementsByClassName("DP-2-pdf-selector")
 	const pdfFrame2 = document.getElementById("DP-2-frame")
 	const pdfLink2 = document.getElementById("DP-2-link")
 	for (const selector of pdfSelectors2) {
-		selector.addEventListener("click", e => {
-			pdfFrame2.src = selector.dataset.src
-			pdfLink2.href = selector.dataset.src
-		})
+		if (selector.dataset.src) {
+			selector.addEventListener("click", e => {
+				pdfFrame2.src = selector.dataset.src
+				pdfLink2.href = selector.dataset.src
+			})
+		}
+	}
+	const pdfSelectors3 = document.getElementsByClassName("DP-3-pdf-selector")
+	const pdfFrame3 = document.getElementById("DP-3-frame")
+	const pdfLink3 = document.getElementById("DP-3-link")
+	for (const selector of pdfSelectors3) {
+		if (selector.dataset.src) {
+			selector.addEventListener("click", e => {
+				pdfFrame3.src = selector.dataset.src
+				pdfLink3.href = selector.dataset.src
+			})
+			// selector.addEventListener("auxclick" e => {
+			// 	// middle click
+			// 	if (e.which === 2) {
+			// 		e.preventDefault()
+			// 		let oldSrc = pdfLink3.href
+			// 		pdfLink3.href = selector.dataset.src
+			// 		pdfLink3.click()
+			// 		pdfLink3.href = oldSrc
+			// 	}
+			// })
+		}
 	}
 })
